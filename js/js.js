e8ef20ce1fe05c5fa61bfa89a8f678ff9ce78b66
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-03-14 03:17:37 -0300

    projeto novo

--- js/js.js
+++ js/js.js
@@ -12,19 +12,18 @@ form.onsubmit = (event) => {
     const weight = inputWeight.value; 
     const height = inputHeight.value;
 
-    const weightOrHeightIsNotNumber = notANumber(weight) || notANumber(height)
+    const showAlertError = notANumber(weight) || notANumber(height)
     
-    if(weightOrHeightIsNotNumber){
+    if(showAlertError){
       AlertError.open();
-      return;;
+      return;
     }
 
     AlertError.close();
-
-      const result = calculateIMC(weight,height);
-      displayResultMessage()
+      
+    const result = calculateIMC(weight,height);
+    displayResultMessage(result)
     };
-    
     
     function displayResultMessage(result) {
       const message = `Seu IMC é de ${result}`
